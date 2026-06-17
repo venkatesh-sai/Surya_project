67491a5dd80a73497a1770e9e556f41e8a3a6fc7
tirumala-thewify <tirumala@thewify.com>
2026-06-17 16:46:16 +0530

    Merge surya into main

--- backend/project/project/settings.py
+++ backend/project/project/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Custom apps
+    'brands',
+    'categories',
+    'products',
+    'enquiries',
+    'testimonials',
 ]
 
 MIDDLEWARE = [
